99034a93fd773599d6e9e72572e079b918a393ee
Sarang-Admin <151469564+Sarang-Admin@users.noreply.github.com>
2023-11-20 02:48:17 +0530

    Update route - Hostel Room

--- src/app/api/users/admin/hostelRoom/route.ts
+++ src/app/api/users/admin/hostelRoom/route.ts
@@ -39,7 +39,7 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(response: NextResponse) {
+export async function GET() {
   try {
     const hostelRooms = await prisma.hostelRoom.findMany();
     return NextResponse.json({ hostelRooms });
